fdb7837938c4a9d48f62b29fe26221cb45cd2165
HaniOlwan <heenoow@gmail.com>
2022-06-08 01:52:43 +0300

    teacher subjects
    created migration with foreign key linking to teacher created the subject
    Relates #14

---
+++ database/migrations/2022_06_07_224410_create_subjects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('code');
+            $table->string('status');
+            $table->string('description');
+            $table->bigInteger('teacher_id')->unsigned()->index()->nullable();
+            $table->foreign('teacher_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+}
