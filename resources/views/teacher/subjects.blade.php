aabae2913fd26bff3bb90db8a1df7bc1cf1fecc4
HaniOlwan <heenoow@gmail.com>
2022-06-10 00:30:45 +0300

    teacher subjects
    add icon to private subjects
    Relates #14

--- resources/views/teacher/subjects.blade.php
+++ resources/views/teacher/subjects.blade.php
@@ -63,7 +63,7 @@
                             <td scope="col">{{$subject->subject_id}}</td>
                             <td scope="col">{{$subject->code}}</td>
                             <td scope="col"><a href="view-subject.php?subject_id= echo $row['id']; ?>#students">particapants</a></td>
-                            <td scope="col"><i class="fas fa-lock-open"> </i>{{$subject->private== 1? "private": "public"}}</td>
+                            <td scope="col"><i class="{{$subject->private== '1' ? 'fas fa-lock' : 'fas fa-lock-open'}}"> </i>{{$subject->private== 1? "private": "public"}}</td>
                             <td scope="col"><a href="edit-subject.php?subject_id= echo $row['id']; ?>"><i class="fas fa-pencil-alt"></i></a></td>
                             <td scope="col"><a onclick="return confirm('Are you sure deleting subject ? \nBy deleting the subject everything related to this subject will be deleted such as topics, questions and quizzes, and you will not be able to recover this data anymore!')" href="subjects.php?delete="><i class="fas fa-trash-alt"></i></a></td>
                         </tr>
