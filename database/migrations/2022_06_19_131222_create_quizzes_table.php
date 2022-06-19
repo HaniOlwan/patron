ac101a6e7ae440f25e2eb131186f088e8b7bb174
HaniOlwan <heenoow@gmail.com>
2022-06-19 16:21:05 +0300

    teacher quizzes
    create migration with quiz data and foriegn keys from quizz to subject and user
    Relates #26

--- database/migrations/2022_06_19_131222_create_quizzes_table.php
+++ database/migrations/2022_06_19_131222_create_quizzes_table.php
@@ -15,6 +15,17 @@ class CreateQuizzesTable extends Migration
     {
         Schema::create('quizzes', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('start_date');
+            $table->string('start_time');
+            $table->string('deadline_date');
+            $table->string('deadline_time');
+            $table->string('duration');
+            $table->string('mark');
+            $table->bigInteger('subject_id')->unsigned()->index()->nullable();
+            $table->foreign('subject_id')->references('id')->on('subjects')->onDelete('cascade');
+            $table->bigInteger('user_id')->unsigned()->index()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
