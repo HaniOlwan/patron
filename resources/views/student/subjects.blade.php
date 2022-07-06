fa37c63a5417d16e48bec1560313e16db77fcede
HaniOlwan <heenoow@gmail.com>
2022-07-06 15:34:41 +0300

    add student joined subjects count, fix subject status display in subjects page Relates #43

--- resources/views/student/subjects.blade.php
+++ resources/views/student/subjects.blade.php
@@ -67,7 +67,7 @@
                             <td scope="col"><a href="/student/view-subject/{{ $subject->id  }}">{{ $subject->title }}</a></td>
                             <td scope="col">{{ $subject->subject_id }}</td>
                             <td scope="col" style="text-transform: capitalize"><a href="/teacher/{{ $subject->teacher->id }}">{{ $subject->teacher->first_name." ".$subject->teacher->last_name }}</a></td>
-                            <td scope="col">{{ $subject->private }}</td>
+                            <td scope="col"><i class="{{$subject->private== '1' ? 'fas fa-lock' : 'fas fa-lock-open'}}"></i> {{$subject->private== '1' ? 'Private' : 'Public'}}</td>
                             <td scope="col"><a href="" class="drop" subject-id="{{ $subject->id }}" data-status="{{ $subject->private }}">Drop</a></td>
                         </tr>
                         @endforeach
